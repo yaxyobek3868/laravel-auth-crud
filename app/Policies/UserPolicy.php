5ec87bd5d894daf672deebfd65a452c44ee03d93
yaxyobek3868 <mahmudov3868@gmail.com>
2026-02-02 17:12:35 +0500

    UserPolicies ozgardi

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -6,31 +6,30 @@ use App\Models\User;
 
 class UserPolicy
 {
-    // Hamma userlar ro‘yxatni ko‘ra oladi
+
     public function viewAny(User $authUser): bool
     {
         return true;
     }
 
-    // Hamma userlar profilni ko‘ra oladi
     public function view(User $authUser, User $user): bool
     {
         return true;
     }
 
-    // Faqat admin user yaratadi
+
     public function create(User $authUser): bool
     {
         return $authUser->role == 1;
     }
 
-    // Admin yoki o‘zi edit qiladi
+
     public function update(User $authUser, User $user): bool
     {
         return $authUser->role == 1 || $authUser->id === $user->id;
     }
 
-    // Faqat admin delete qiladi
+
     public function delete(User $authUser, User $user): bool
     {
         return $authUser->role == 1;
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,7 +13,10 @@
         @can('create', App\Models\User::class)
         <a href="{{ route('users.create') }}" class="btn btn-success ms-2">
             Add User
-            ...
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
         </a>
     @endcan
     </a>
@@ -44,10 +47,8 @@
             <td>{{ $user->date_of_birth }}</td>
             <td>{{ $user->address }}</td>
             <td>
-
                 <a href="{{ route('users.show', $user) }}" class="btn btn-info btn-sm">
-                    View
-                </a>
+                    View</a>
                     @can('update', $user)
                         <a href="{{ route('users.edit', $user) }}" class="btn btn-warning btn-sm">Edit</a>
                     @endcan
